708f7cb4180257a983598854b5ae2101674223fa
Ramin B <i8ramin@gmail.com>
2015-11-22 21:48:09 -0500

    make it auth

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Rails.application.config.session_store :cookie_store, key: '_karlandkeke_session'
-Rails.application.config.session_store :cache_store
+Rails.application.config.session_store :cache_store, expire_after: 1.week
