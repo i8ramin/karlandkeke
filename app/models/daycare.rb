6565c86eee920e651924f9126939928f4f29b9a0
David Cascino <david@thunderclap.it>
2015-12-23 13:53:49 -0500

    error checking during import script, latest data scrape

--- app/models/daycare.rb
+++ app/models/daycare.rb
@@ -8,7 +8,7 @@ class Daycare
   field :permit_holder, type: String
   field :address, type: String
   field :borough, type: String
-  field :zipCode, type: String
+  field :zipcode, type: String
   field :phone, type: String
   field :permit_status, type: String
   # should permit number be an int, does it matter?
@@ -25,6 +25,21 @@ class Daycare
     d = Daycare.new
     d.type = payload["type"]
     d.center_name= payload["centerName"]
+    d.permit_holder = payload["permitHolder"]
+    d.address = payload["address"]
+    d.borough = payload["borough"]
+    d.zipcode = payload["zipCode"]
+    d.phone = payload["phone"]
+    d.permit_status = payload["permitStatus"]
+    d.permit_number = payload["permitNumber"]
+    d.permit_expiration_date = payload["permitExpirationDate"]
+    d.age_range = payload["ageRange"]
+    d.maximum_capacity = payload["maximumCapacity"]
+    d.site_type = payload["siteType"]
+    d.certified_to_administer_medication = payload["certifiedToAdministerMedication"]
+    d.years_operating = payload["yearsOperating"]
+    d.has_inspections = payload["hasInspections"]
+
     i = Inspection.from_json(payload["latestInspection"])
     d.inspection = i
     i.daycare = d
--- lib/tasks/import_json.rake
+++ lib/tasks/import_json.rake
@@ -3,6 +3,7 @@ task :import_json => :environment do
 
     puts "BEGIN import JSON --> Mongo"
     counter = 0
+    fails = []
 
     # makes sense to clear out the existing data in our case
     Infraction.destroy_all
@@ -14,11 +15,20 @@ task :import_json => :environment do
     venues = JSON.parse(file)
     puts venues.size
     venues.each do |daycare|
+        begin
         d = Daycare.from_json(daycare)
-        puts d.inspect
+        counter += 1
+        rescue Exception => e
+            fails << daycare
+            puts "FAILED TO PARSE DAYCARE"
+            puts e.to_s
+            puts daycare.inspect
+        end
+
     end
 
     puts "#{counter} records imported"
+    puts "#{fails.size} failed to import"
     puts "END   import JSON --> Mongo"
 
 end
