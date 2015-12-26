a148b657ca6d3e77785aa387771b46b668851526
Harish Krishna <harish.krishna@wework.com>
2015-12-26 01:42:04 -0500

    adding basic map (mapbox/leaflet), using mapbox-gl

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,21 @@
 //= require_tree .
 
 //= require semantic_ui/semantic_ui
+//= require mapbox/mapbox-gl_v0.12.2
+//= require leaflet/leaflet
+//= require leaflet/leaflet_hash
 
 $('.ui.dropdown').dropdown();
+
+// setup map
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGsyMyIsImEiOiJjaWg2dDRmOXAwNmNpdWtrdDRvdW1xdzI2In0.F9uULd8DhCkRGltilPPZbg';
+
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/hk23/ciimpkraf007n8ulx593l7qze', //stylesheet location
+    center: [-74.0059, 40.7127], // starting position 40.7127° N, 74.0059° W
+    zoom: 9 // starting zoom
+});
+
+// TODO setup hash (for Mapbox GL)
+// var hash = L.hash(map);
