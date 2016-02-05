60a12e2cd2e3ff58386a14fa60c63f3d5dc3f176
Alex Silverman <alex@silverswatch.com>
2016-02-05 03:24:39 +0000

    got a full latest inspection, though some cleaning issues

--- data/nys_scraper.rb
+++ data/nys_scraper.rb
@@ -6,8 +6,6 @@ require 'json'
 require 'mechanize'
 require_relative 'cleanup.rb'
 
-DEMO = true
-
 centerTypes = {
 	'FDC' =>'Family Day Care',
 	'GFDC' => 'Group-Family Day Care',
@@ -52,13 +50,10 @@ siteCrawler = Mechanize.new
 # CSV setup
 csv_file = nil
 puts "getting CSV..."
-if DEMO
-	csv_file = File.open("nys_childcare.csv", "r") { |file| file.read  }
-else
-	csv_url = 'https://data.ny.gov/api/views/cb42-qumz/rows.csv'
-	crawlPage = siteCrawler.get(csv_url)
-	csv_file = crawlPage.body
-end
+csv_file = File.open("nys_childcare.csv", "r") { |file| file.read  }
+# csv_url = 'https://data.ny.gov/api/views/cb42-qumz/rows.csv'
+# crawlPage = siteCrawler.get(csv_url)
+# csv_file = crawlPage.body
 puts "--got CSV"
 
 
@@ -76,77 +71,106 @@ puts "--initial parse complete"
 puts "PARSE PT. 2..."
 
 daycares = []
+all_violations = []
 i = 0
 while i < 20
 	daycare = daycare_ls[i]
-# above for demo'ing purposes, below for real scrape
+# above^ for demo'ing purposes, below*v for full scrape
 # daycare_ls.each_with_index do |daycare, i|
 
-	if BOROUGHS.include? daycare["County"]
+	if i % 100 == 0 
 		puts i
-		# puts daycare
-		DROPLIST.each do |drop|
-			daycare.delete(drop)
+	end
+	
+	# get rid of some unnecessary fields. Might slow things down a bit
+	DROPLIST.each do |drop|
+		daycare.delete(drop)
+	end
+
+	# a bunch of renames / sometimes formatting changes
+	daycare['type'] = centerTypes[daycare.delete('Program Type')]
+	daycare['permitStatus'] = daycare.delete("Facility Status")
+	daycare['permitExpirationDate'] = daycare.delete("License Expiration Date")
+	daycare['borough'] = daycare.delete('County')
+	daycare['zipCode'] = daycare.delete("Zip Code")
+	daycare['address'] = "#{daycare.delete('Street Number')} #{daycare.delete('Street Name')}"
+	daycare['centerName'] = daycare.delete("Facility Name")
+	daycare['permitHolder'] = daycare.delete("Provider Name")
+	daycare['latitude'] = daycare.delete("Latitude").to_i
+	daycare['longitude'] = daycare.delete("Longitude").to_i
+	daycare['phone'] = daycare.delete("Phone Number")
+	ageRange_start = daycare.delete("Capacity Description")
+	if !ageRange_start.nil?
+		daycare['ageRange'] = ageRange_start.split(", ")[1].gsub(/Ages | years/, '').gsub('to','-')
+	end
+
+
+	# actual scraping
+	site = nil
+	while site.nil? do
+		begin
+			site = siteCrawler.get daycare['Program Profile']
+		rescue
+			puts "error getting page. trying again"
 		end
+	end
 
-		# a bunch of renames / sometimes formatting changes
-		daycare['type'] = centerTypes[daycare.delete('Program Type')]
-		daycare['permitStatus'] = daycare.delete("Facility Status")
-		daycare['permitExpirationDate'] = daycare.delete("License Expiration Date")
-		daycare['borough'] = daycare.delete('County')
-		daycare['zipCode'] = daycare.delete("Zip Code")
-		daycare['address'] = "#{daycare.delete('Street Number')} #{daycare.delete('Street Name')}"
-		daycare['centerName'] = daycare.delete("Facility Name")
-		daycare['permitHolder'] = daycare.delete("Provider Name")
-		daycare['latitude'] = daycare.delete("Latitude").to_i
-		daycare['longitude'] = daycare.delete("Longitude").to_i
-		daycare['phone'] = daycare.delete("Phone Number")
-		ageRange_start = daycare.delete("Capacity Description")
-		
-		if !ageRange_start.nil?
-			daycare['ageRange'] = ageRange_start.split(", ")[1].gsub(/Ages | years/, '').gsub('to','-')
-		end
+	inspectionHistory = site.search('#compliancehistoryDivImg')[0]
+	inspectionTables = inspectionHistory.search('table')
+	inspectionTop = inspectionTables[0]
+	inspectionBottom = inspectionTables[1]
 
-		# actual scraping
-		site = siteCrawler.get daycare['Program Profile']
-		inspectionHistory = site.search('#compliancehistoryDivImg')[0]
-		inspectionTables = inspectionHistory.search('table')
-		inspectionTop = inspectionTables[0]
-		inspectionBottom = inspectionTables[1]
+	latest_inspection = {}
+	latest_inspection['date'] = inspectionTop.search('td')[0].text.split(': ')[1]
+	latest_inspection['result'] = inspectionTop.search('td')[1].text.split('violations:' )[1]
+	latest_inspection['infractions'] = []
+	latest_inspection['numInfractions'] = 0
+	latest_date_split = latest_inspection['date'].split(" ")
+	latest_date_split[0] = Date::MONTHNAMES.index(latest_date_split[0])
+	latest_date = latest_date_split.join "/"
 
-		latest_insp = {}
-		latest_insp['date'] = inspectionTop.search('td')[0].text.split(': ')[1]
-		latest_insp['result'] = inspectionTop.search('td')[1].text.split('violations:' )[1]
-		daycare['latestInspection'] = latest_insp
 
-		if inspectionBottom
-			violations_table = inspectionBottom.search('tr')
-			violations_table.delete violations_table.first # headers
+	# if will be false if table doesn't exist
+	if inspectionBottom
+		violations_table = inspectionBottom.search('tr')
+		violations_table.delete violations_table.first # headers
 
-			violations = []
-			violations_table.each do |violation_node|
-				violation = {}
-				sections = violation_node.search('td')
-				VIOLATION_HEADERS.each_with_index do |header, header_i|
-					violation[header.downcase] = sections[header_i]
-				end
-				date_spl = violation["date"].text.split(" ")
-				date_spl[0] = Date::MONTHNAMES.index(date_spl[0])
-				violation["date"] = date_spl.join "/"
+		violations = []
+		violations_table.each do |violation_node|
+			violation = {}
+			sections = violation_node.search('td')
+			VIOLATION_HEADERS.each_with_index do |header, header_i|
+				violation[header.downcase] = sections[header_i]
+			end
+			date_spl = violation["date"].text.split(" ")
+			date_spl[0] = Date::MONTHNAMES.index(date_spl[0])
+			violation["date"] = date_spl.join "/"
+			if latest_date == violation["date"]
+				latest_inspection['infractions'] << violation
+				latest_inspection['numInfractions'] += 1
+			else
 				violations << violation
 			end
-
-			daycare['violations'] = violations
 		end
 
-
-
-		daycares << cleanup(daycare)
-		i += 1
+		daycare['latestInspection'] = latest_inspection
+		daycare['violations'] = violations
+		all_violations += violations
 	end
+
+	daycares << cleanup(daycare)
+	i += 1
 
 end
 
 File.open("json/nys_daycares.json", "w") do |f|
 	f.write(JSON.pretty_generate(daycares))
 end
+
+File.open("nys_violations.csv", "w") do |f|
+	all_violations.each do  |v|
+		f.write v.values.join(",")+"\n"
+	end
+end
+
+
